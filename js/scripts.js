2920a9fdb31ed4752e5dea511fd4cd062382b562
Elgars <elgars.ab@gmail.com>
2025-10-18 13:58:51 +0300

    More advanced jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -68,6 +68,67 @@
 		$topBtn.on('click', function () {
 			$('html, body').animate({ scrollTop: 0 }, 420);
 		});
+
+		/* ---- Inline flip-cards for specific headings ---- */
+		(function () {
+			var ids = ['csspreprocessor', 'sass', 'howtousesass', 'howtosass'];
+			ids.forEach(function (id) {
+				var $heading = $('#' + id);
+				if (!$heading.length) return;
+
+				// Collect following paragraphs until next heading of same-or-higher level
+				var $content = $();
+				var $el = $heading.next();
+				while ($el.length && !$el.is('h1,h2,h3,h4,h5,h6')) {
+					if ($el.is('p,ul,ol,pre')) $content = $content.add($el);
+					$el = $el.next();
+				}
+
+				// If there's no paragraph content, skip
+				if (!$content.length) return;
+
+				// Build flip card pieces
+				var $card = $('<div class="flip-card" tabindex="0" role="button" aria-pressed="false"></div>');
+				var $inner = $('<div class="flip-card-inner"></div>');
+				var $front = $('<div class="flip-card-front"></div>');
+				var $back = $('<div class="flip-card-back"></div>');
+
+				// Move heading into front (clone to preserve original if needed)
+				$front.append($heading.clone().addClass('front-heading'));
+
+				// Move content elements into back
+				$content.each(function () { $back.append($(this).clone()); });
+
+				$inner.append($front).append($back);
+				$card.append($inner);
+
+				// Replace heading and content with card
+				$content.first().before($card);
+				// remove original content and heading
+				$heading.remove();
+				$content.remove();
+
+				// Toggle function
+				function toggleFlip($c) {
+					var is = $c.hasClass('is-flipped');
+					$c.toggleClass('is-flipped', !is);
+					$c.attr('aria-pressed', String(!is));
+					$c.find('.flip-card-inner').toggleClass('is-flipped', !is);
+				}
++
+				// Click and keyboard handlers
+				$card.on('click', function (e) {
+					e.preventDefault();
+					toggleFlip($card);
+				});
+				$card.on('keydown', function (e) {
+					if (e.key === 'Enter' || e.key === ' ') {
+						e.preventDefault();
+						toggleFlip($card);
+					}
+				});
+			});
+		})();
 	});
 })();
 
